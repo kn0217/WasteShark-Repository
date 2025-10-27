417f8c0dc7873a6ad137e94e829bac4f6e0c64ee
kardin <knkardin@gmail.com>
2025-10-27 16:30:18 -0700

    test navbar 10/27/2025

--- frontEnd/src/components/Navbar.jsx
+++ frontEnd/src/components/Navbar.jsx
@@ -1,7 +1,19 @@
+import { Link, useResolvedPath, useMatch} from "react-router-dom"
+
 const Navbar = () => {
-  return (
-    <div>"Hello"</div>>
- )
+  return <nav className="navbar">
+    <Link to="/" className="navbar-brand">
+      WasteShark
+    </Link>
+    
+    <Link to="/" className="nav-item">
+      Home  
+    </Link>
+
+    <Link to="/login" className="nav-item">
+      Login
+    </Link>
+  </nav>
 }
 
 export default Navbar
