ec06260c5f7e100523b1d3d8d5183652c814b29d
kardin <knkardin@gmail.com>
2025-10-27 19:50:00 -0700

    navbar v2 10/27/2025

--- frontEnd/src/components/Navbar.jsx
+++ frontEnd/src/components/Navbar.jsx
@@ -1,19 +1,22 @@
-import { Link, useResolvedPath, useMatch} from "react-router-dom"
+import { Link } from "react-router-dom"
+import { ReactComponent as WasteSharkLogo } from '../assets/logo_transparent.svg'
 
 const Navbar = () => {
-  return <nav className="navbar">
-    <Link to="/" className="navbar-brand">
-      WasteShark
-    </Link>
-    
-    <Link to="/" className="nav-item">
-      Home  
-    </Link>
+  return (
+    <nav className="flex items-center justify-between p-6 bg-navy text-white shadow-md">
+      {/* Logo Section */}
+      <div className="flex items-center">
+        <WasteSharkLogo className="h-10 w-10 mr-3" />
+        <span className="text-2xl font-bold">WasteShark</span>
+      </div>
 
-    <Link to="/login" className="nav-item">
-      Login
-    </Link>
-  </nav>
+      {/* Navigation Links */}
+      <div className="flex-1">
+        <Link to="/" className="nav-item mr-6">Home</Link>
+        <Link to="/login" className="nav-item">Login</Link>
+      </div> {/* Spacer to push links to the right */}
+    </nav>
+  )
 }
 
 export default Navbar
